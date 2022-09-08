f8263b34f0b25a4ef0f3900b5a136894ea59e404
berti <berti@epse.be>
2022-09-08 13:32:23 +0200

    commentaires

--- pages/produit.php
+++ pages/produit.php
@@ -15,11 +15,20 @@
                 <h1>Les avis</h1>
             </div>
         <?php
-     
+            $coms = $bdd->prepare("SELECT commentaires.texte AS ctexte, DATE_FORMAT(commentaires.date, '%d/%m/%Y %Hh%i') AS mydate, membre.login AS mlogin, commentaires.id AS cid FROM commentaires INNER JOIN membre ON commentaires.id_membre = membre.id WHERE commentaires.id_produit=? ORDER BY commentaires.date DESC");
+            $coms->execute([$id]);
+            while($donComs = $coms->fetch())
+            {
+                echo "<div class='coms'>";
+                    echo "<div class='comsInfo'>";
+                        echo "<a href='#' class='author'>";
+                            echo $donComs['mlogin'];
+                        echo "</a>";
+                        echo "<div class='date'>".$donComs['mydate']."</div>";
+                    echo "</div>";
+                    echo "<div class='comTxt'>".nl2br($donComs['ctexte'])."</div>";
+                echo "</div>";
+            }
+            $coms->closeCursor();
         ?>    
-        
-
-        
-   
-
 </div>
