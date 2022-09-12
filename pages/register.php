cc6905dc66fd8d93c81fb598611d7fa756f1cdf5
berti <berti@epse.be>
2022-09-12 09:59:03 +0200

    inscription V2

--- pages/register.php
+++ pages/register.php
@@ -1,1 +1,18 @@
 <h1>Inscription</h1>
+<div class="container">
+    <form action="#" method="POST">
+        <div class="form-group">
+            <input type="text" name="login" id="login" placeholder="Login">
+        </div>
+        <div class="form-group">
+            <input type="password" name="password" id="password" placeholder="Mot de passe">
+        </div>
+        <div class="form-group">
+            <input type="email" name="email" id="email" placeholder="Votre adresse E-mail">
+        </div>
+        <div class="form-group">
+            <input type="submit" value="Enregistrer">
+        </div>
+    </form>
+</div>
+
